a9215899302ca02ba49e1e305beff83d0762c77c
Xyp1ce <ramdev4559@gmail.com>
2025-04-12 00:10:23 -0700

    x

--- Taller9ManriquezRamon.c
+++ Taller9ManriquezRamon.c
@@ -115,6 +115,7 @@ void reducir(int **ptr, int *longitud){
 void ordenar(int *ptr, int *longitud){
 	int aux = 0;
 	int ordenado = 0;
+	// Bubble Sort
 	while(ordenado == 0){
 		ordenado = 1;
 		for(int i = 0; i < *longitud - 1; i++){	
